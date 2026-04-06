e7e474e3f55f47d5b95a5ea6eb65a8f2ccfe1400
Adolfovich <adolfovich.alexashka@gmail.com>
2026-04-06 20:40:23 +0300

    fix TG bot

--- helperBot/bot.php
+++ helperBot/bot.php
@@ -1,8 +1,5 @@
 <?php
-// Конфигурация
-define('BOT_TOKEN', '8607364179:AAGcw64Yr9JM7kIC86NzgzS8oMLomEepac8'); // Замените на токен вашего бота
-define('API_URL', 'https://api.telegram.org/bot' . BOT_TOKEN . '/');
-define('EXTERNAL_API_URL', 'http://vsk.exeptional.ru/api/helper/');
+include ('_config.php');
 
 // Получаем обновления от Telegram
 function getUpdates($offset = 0) {
